c7ef061136033e5f131e71f215f49d5fea70b45c
Marvin-Baafi-Owusu-hub <owusumarvin77.com>
2026-02-27 13:13:12 +0000

    i deleted some of the commits in quizengine.tsx

--- src/pages/QuizEngine.tsx
+++ src/pages/QuizEngine.tsx
@@ -25,7 +25,7 @@ export const QuizEngine = () => {
     <div className="min-h-screen bg-slate-950 text-white flex flex-col items-center p-4 md:p-10">
       
       <main className="max-w-2xl w-full">
-        {/* Header with Timer */}
+       
         <header className="flex justify-between items-end mb-4 px-2">
           <div className="flex flex-col">
             <span className="text-slate-500 uppercase tracking-widest text-[10px] font-bold">Current Vault Path</span>
@@ -34,7 +34,6 @@ export const QuizEngine = () => {
           <CountdownClock onExpire={() => navigate('/summary', { state: { score } })} />
         </header>
 
-        {/* PROGRESS BAR MOVED HERE (Horizontal Top) */}
         <ProgressTracker current={currentStep} total={ITChallengeQuestions.length} />
 
         <AnimatePresence mode="wait">
@@ -43,9 +42,8 @@ export const QuizEngine = () => {
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -20 }}
-            className="bg-slate-900 p-6 md:p-8 rounded-2x1 border border-slate-800 shadow-2xl relative overflow-hidden"
-          >
-            {/* Ambient Background Glow for the Card */}
+            className="bg-slate-900 p-6 md:p-8 rounded-2x1 border border-slate-800 shadow-2xl relative overflow-hidden">
+          
             <div className="absolute -top-24 -right-24 w-48 h-48 bg-cyan-500/10 blur-[80px] rounded-full" />
             
             <h2 className="text-xl md:text-2xl font-bold mb-8 leading-tight relative z-10">
